bf61c63c6c2e11e1717683a687e8cf49712b8e52
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-04 00:25:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
   end 
   
   def self.find_by_name(song_name)
-    @@all.find(){|name| puts name if name == song_name }
+    @@all.find(){|song| song.name == song_name }
   end 
 
 end
